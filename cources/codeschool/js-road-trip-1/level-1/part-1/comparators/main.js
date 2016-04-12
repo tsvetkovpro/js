ee528154ad9cf9eccade48c1ca9beb1d7dab5412
tsvetkovpro <285019@gmail.com>
2016-04-12 10:02:04 +0500

    codeschool js-trip-1 level-1 - Variable Valley; Review how to store and use data with variables.
    Common Operators used in JavaScript Syntax:
    addition
    subtraction
    multiplication
    division
    modulus - Modulus returns the remainder after division.
    
    Grouping Expressions in JavaScript
    
    Common Number Comparators used in JavaScript Syntax:
    >, ==, <, !=, >=, <=
    
    Strings, Special characters inside Strings, String Comparisons
    
    Variables:
    storing, naming variables, changing variable contents, using, incrementing and decrementing

---
+++ cources/codeschool/js-road-trip-1/level-1/part-1/comparators/main.js
@@ -0,0 +1,27 @@
+function msg(message, arg) {
+    return console.log(message, arg);
+}
+
+function sp(arg) {
+    return console.log(arg);
+}
+
+
+msg(6 > 4);
+sp('\n');
+
+msg(9 < 5);
+sp('\n');
+
+msg(3 == 4);
+sp('\n');
+
+msg(12 != 4);
+sp('\n');
+
+msg(8 >= -2);
+sp('\n');
+
+msg(10 <= 10);
+sp('\n');
+
---
+++ cources/codeschool/js-road-trip-1/level-1/part-2/strings/main.js
@@ -0,0 +1,64 @@
+function msg(message) {
+    return console.log(message);
+}
+
+function sp(arg) {
+    return console.log(arg);
+}
+
+msg("raindrops on roses");
+console.log("raindrops" + " and " + "123");
+
+
+// these are a few of my favorite ...strings
+
+console.log("raindrops " + 42);
+console.log("raindrops " + 9 + " and " + 3/4);
+console.log("raindrops " + 9 + " and  3/4");
+
+
+
+// special characters inside strings
+
+console.log("flight #:\t921\t\tSeat:\t21c");
+console.log("login password:\t\t\"CCC\"");
+
+console.log("origin\\destination:\torlando\\london");
+console.log("depature:\t09:55a\nArrival:\t14:55");
+
+
+
+//string comparisons
+
+console.log("the right" == "the right");
+console.log("the right" == "the right brother");
+console.log("the right" != "The Right Brother");
+
+
+
+var s = "dasdasdasdasdsadasdasd12342422";
+msg(s.length);
+
+
+var s1 = "one two three, four qwqe";
+msg(s1.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
