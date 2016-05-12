efad5d7552a53ffb2a08bcc34989087666dcb369
tsvetkovpro <285019@gmail.com>
2016-05-12 16:37:59 +0500

    specialist js level-3 - jQuery
    end cource:
    
     Создание кода плавного показа картинок
     Оптимизация кода
     Создание шаблонизатора
     Использование шаблонизатора Handlebars

--- cources/specialist/уровень-3/module-4/slide-show.js
+++ cources/specialist/уровень-3/module-4/slide-show.js
@@ -1,1 +1,35 @@
-//
+(function ($) {
+    var viewUL = $('div.view')
+        .css('overflow', 'hidden')
+        .children('ul');
+
+    var imgs = viewUL.find('img');
+    var imgW = 400; // можно и лучше:  1- imgs[0].width()   2- imgs.first().width()
+    var imgsLen = imgs.length;
+    var totalImgsW = imgW * imgsLen, current = 1;
+
+    $('#show').show()
+        .find('button').on('click', function () {
+        var direction = $(this).attr('id');
+        var position = imgW;
+        (direction === 'next') ? ++current : --current;
+        if (current === 0) {
+            current = imgsLen;
+            direction = 'next';
+            position = totalImgsW - imgW;
+        } else if (current - 1 === imgsLen) {
+            current = 1;
+            position = 0;
+        }
+        doIt(viewUL, position, direction);
+    });
+
+    function doIt(container, position, direction) {
+        var sign;  // -= +=
+        if (direction && position !== 0) {
+            sign = (direction === 'next') ? '-=' : '+=';
+        }
+        container.animate({'margin-left': sign ? (sign + position) : position});
+    }
+
+})(jQuery);
