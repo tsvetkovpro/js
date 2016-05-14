52bab3dc9ce14880fc5668f5316b1d334bb46ed3
tsvetkovpro <285019@gmail.com>
2016-05-14 21:25:06 +0500

    codeschool es6 - level-1 - Declarations with let
    Understanding Hoisting
    Prior to executing our code, JavaScript moves var declarations all the way up
    to the top of the scope. This is known as hoisting.
    
    let variables are scoped to the nearest block and are NOT hoisted.
    (A block is any code section within curly braces, like if, else, for, while, etc.)
    
    Using let, variables are “trapped” inside their respective if and else blocks.

---
+++ cources/codeschool/es2015/level-1/part-1/declaration-with-let/main.js
@@ -0,0 +1,12 @@
+"use strict";
+//let variables are scoped to the nearest block and are NOT hoisted.
+// (A block is any code section within curly braces, like if, else, for, while, etc.)
+
+function loadProfiles(userNames){
+    if (userNames.length > 3) {
+        let loadingMessage = "This might take a while...";
+        console.log(flashMessage);
+    } else {
+        let flashMessage = "Loading profiles";
+    }
+}
