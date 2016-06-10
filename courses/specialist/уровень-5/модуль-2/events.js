5ca644fcc989f5bff3ed1d7c283be985eb961131
tsvetkovpro <285019@gmail.com>
2016-06-10 16:38:43 +0500

    specialist js level-5 NodeJS
    end module-2 - События
    
    События JavaScript
    	Встроенный вызов
    	Пользовательский вызов
    События http-сервера Node.js
    Пользовательские события Node.js

---
+++ courses/specialist/уровень-5/модуль-2/events.js
@@ -0,0 +1,25 @@
+﻿var events = require('events');
+var EventEmitter = events.EventEmitter;
+
+/*console.log(EventEmitter);
+ { [Function: EventEmitter]
+ EventEmitter: [Circular],
+ usingDomains: false,
+ defaultMaxListeners: [Getter/Setter],
+ init: [Function],
+ listenerCount: [Function] }
+*/
+
+var test = new EventEmitter();
+test.on('myEvent', onMyEvent);
+
+function onMyEvent (param) {
+	console.log( arguments[0], arguments[1], arguments[2] ); // console.log(param.name, param.age); Ivan 21
+}
+
+test.emit('myEvent', 'one', 'two', 'three');
+
+/*test.emit('myEvent', {name:'Ivan', age:21} );*/
+/*
+test.emit('myEvent', '1');
+test.emit('myEvent', '2');*/
