615a68578e60b8e6fd2ec18ae221eb4c580802e3
tsvetkovpro <285019@gmail.com>
2016-06-20 12:31:43 +0500

    specialist js level-5 NodeJS end module-5 - Socket.IO
    Взаимодействие клиент –сервер
    Обзор модели работы Comet
    Модуль Socket.IO
    Совместное использование модулей
       - Express и Socket.IO
    Основные методы и свойства Socket.IO

---
+++ courses/specialist/level-5/module-6/chat/_index.js
@@ -0,0 +1,83 @@
+﻿var express = require('express');
+var app = express();
+var port = 8088;
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('page');
+});
+
+var io = require('socket.io').listen(app.listen(port));
+
+var users = {};
+function getUser(obj) {
+    var tmp = [];
+    for (var i in obj) {
+        tmp.push(obj[i]);
+    }
+    return tmp.join(', ');
+}
+
+io.sockets.on('connection', function (client) {
+    //console.log('сокет он');
+    //client.emit('message', {message: 'Добро пожаловать в чат!'});
+    client.on('send', function (data) {
+        io.sockets.emit('message', {message:data.message}); // всем
+    });
+
+    client.on('privetstvie', function (data) {
+        client.set('nickname', data.name);
+        client.emit('message', {message: '... Добро пожаловать в чат ' + data.name + '...'});
+        client.broadcast.emit('message', {message: '...' + data.name + ' присоеденился к чату ...'});
+
+        if (Object.keys(users).length > 0) {
+            var userList = getUser(users);
+            client.emit('message', {message: '... Уже в чате: ' + userList + '...'});
+        } else {
+            client.emit('message', {message: '... Кроме Вас, в чате никого нет ...'});
+        }
+        users[client.id] = data.name;
+    });
+
+    client.on('disconnect', function (data) {
+        if (Object.keys(users).length > 1) { // если клиент не единственный - последний то бишь
+            client.get('nickname', function (err, name) {
+                client.broadcast.emit('message', {message: '...' + name + ' покинул чат'});
+            });
+        }
+        delete users[client.id];
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ courses/specialist/level-5/module-6/chat/public/chat.js
@@ -0,0 +1,40 @@
+﻿var socket;
+
+window.onunload = function () {
+    socket.disconnect();
+};
+
+window.onload = function () {
+    socket = io.connect('http://localhost:8088');
+
+    var field = document.getElementById('field');
+    var form = document.getElementById('form');
+    var content = document.getElementById('content');
+
+    var name = prompt('Как Вас зовут?', 'Гость');
+    if (name) {
+     socket.emit('privetstvie', {name: name});
+    }
+
+    form.onsubmit = function () {
+        var text = field.value; // - выбрать введённые пользователем данные из текстового поля
+        socket.emit('send', {message: text}); // - послать выбранные данные на сервер в сообщении под именем 'send'
+        return false; // reload page off
+    };
+
+    var messages = [];
+
+    socket.on('message', function (data) {
+        if (data.message) {
+            messages.push(data.message);
+            var html = '';
+            for (var i = 0; i < messages.length; i++) {
+                html += messages[i] + '<br/>';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log('Some error ...');
+        }
+    });
+};
+
