1ff0d537c7c05427e78bfcaab1a26a485e1cdae7
tsvetkovpro <285019@gmail.com>
2016-06-24 08:18:22 +0500

    start laracast es6 series

---
+++ courses/others/-laracasts/src/index.js
@@ -0,0 +1,11 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        return this.name + ' привет!...';
+    }
+}
+
+console.log(new Person('Jeffrey').greet());
