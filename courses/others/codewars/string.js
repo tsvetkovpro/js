c8125d6556775f0d2f15b4dabcabcfa263121679
tsvetkovpro <285019@gmail.com>
2016-10-18 10:22:06 +0500

    + увеличить каждый первый символ в слове:
    Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
    Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"

--- courses/others/codewars/string.js
+++ courses/others/codewars/string.js
@@ -1,6 +1,4 @@
 // reverseWords("This is an example!"); // returns "sihT si na !elpmaxe"
-
-
 function reverseWords(str) {
 	str = str.split(" ");
 	var res = [];
@@ -28,3 +26,59 @@ const reverseWords3 = (str) => str
 	.map(word => word.split('').reverse().join(''))
 	.join(' ');
 console.log(reverseWords3("This is an example!"));
+
+
+
+
+
+// string.replace - /`/g ---> <code> </code>
+let count = 0;
+const input = 'Run `npm install`, then `npm start`';
+
+const output = input.replace(/`/g, () => {
+	count++;
+  if (count % 2) {
+    return '<code>'
+  }
+  return '</code>';
+});
+
+// or if I wanted to show off my mad one-liner skillz
+// const output = input.replace(/`/g, () => ++count % 2 ? '<code>' : '</code>');
+//             document.querySelector('#output').innerHTML = output;
+
+
+console.log('\n');
+
+
+
+
+// Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+// Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+String.prototype.toJadenCase = function () {
+	var arr = this.split(' ');
+	return arr.map(function(el) {
+		return el.charAt(0).toUpperCase() + el.slice(1);
+	}).join(' ');
+};
+var str = "How can mirrors be real if our eyes aren't real";
+console.dir(str.toJadenCase());
+
+
+// best solution
+String.prototype.toJadenCase2 = function () {
+	return this.split(" ").map(function(word){
+		return word.charAt(0).toUpperCase() + word.slice(1);
+	}).join(" ");
+};
+var str2 = "How can mirrors be real if our eyes aren't real";
+console.dir(str2.toJadenCase2());
+
+
+
+
+
+
+
+
+
