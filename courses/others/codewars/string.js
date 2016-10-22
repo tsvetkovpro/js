9986d7e38dbdd2dca3e15c4087dd57a4ab07aa2c
tsvetkovpro <285019@gmail.com>
2016-10-22 12:24:15 +0500

    Best Practices

--- courses/others/codewars/string.js
+++ courses/others/codewars/string.js
@@ -28,19 +28,16 @@ const reverseWords3 = (str) => str
 console.log(reverseWords3("This is an example!"));
 
 
-
-
-
 // string.replace - /`/g ---> <code> </code>
 let count = 0;
 const input = 'Run `npm install`, then `npm start`';
 
 const output = input.replace(/`/g, () => {
 	count++;
-  if (count % 2) {
-    return '<code>'
-  }
-  return '</code>';
+	if (count % 2) {
+		return '<code>'
+	}
+	return '</code>';
 });
 
 // or if I wanted to show off my mad one-liner skillz
@@ -51,13 +48,11 @@ const output = input.replace(/`/g, () => {
 console.log('\n');
 
 
-
-
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 String.prototype.toJadenCase = function () {
 	var arr = this.split(' ');
-	return arr.map(function(el) {
+	return arr.map(function (el) {
 		return el.charAt(0).toUpperCase() + el.slice(1);
 	}).join(' ');
 };
@@ -67,7 +62,7 @@ console.dir(str.toJadenCase());
 
 // best solution
 String.prototype.toJadenCase2 = function () {
-	return this.split(" ").map(function(word){
+	return this.split(" ").map(function (word) {
 		return word.charAt(0).toUpperCase() + word.slice(1);
 	}).join(" ");
 };
@@ -75,12 +70,9 @@ var str2 = "How can mirrors be real if our eyes aren't real";
 console.dir(str2.toJadenCase2());
 
 
-
-
-
 // Нужно написать функцию на Javascript которая бы вставляла символ...
 function star(string, token) {
-	return string.replace(new RegExp(token, 'g'), '*'+token+'*');
+	return string.replace(new RegExp(token, 'g'), '*' + token + '*');
 }
 
 console.log(star('abcabcbbahebbb', 'bb'));
@@ -89,14 +81,11 @@ console.log(star('abcdhklmalh', 'h'));
 
 // Либо - разбить строку на подстроки по разделителю token и сложить обратно с другим разделителем *token*:
 function star(string, token) {
-	return string.split(token).join('*'+token+'*');
+	return string.split(token).join('*' + token + '*');
 }
 
 console.log(star('abcabcbbahebbb', 'bb'));
 console.log(star('abcdhklmalh', 'h'));
-
-
-
 
 
 // Вернуть середину строки js
@@ -126,6 +115,12 @@ console.log(getMiddle("test"));
 console.log(getMiddle("testing"));
 console.log(getMiddle("middle"));
 console.log(getMiddle("A"));
+
+
+// best practies
+function getMiddle(s) {
+	return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
 
 
 
