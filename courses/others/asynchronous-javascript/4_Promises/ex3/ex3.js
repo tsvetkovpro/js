7bd7adb21c28a5b839442df588b399e1637c6f78
tsvetkovpro <285019@gmail.com>
2017-02-02 21:05:03 +0500

    asynchronous-javascript - 4_Promises
     - Exercise_3

--- courses/others/asynchronous-javascript/4_Promises/ex3/ex3.js
+++ courses/others/asynchronous-javascript/4_Promises/ex3/ex3.js
@@ -1,4 +1,4 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
 		"file1": "The first text",
 		"file2": "The middle text",
@@ -8,9 +8,9 @@ function fakeAjax(url,cb) {
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function() {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -20,8 +20,27 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	return new Promise(function executor(resolve, reject) {
+		fakeAjax(file, resolve);
+	});
 }
 
 // request all files at once in "parallel"
-// ???
+
+var p1 = getFile('file1');
+var p2 = getFile('file2');
+var p3 = getFile('file3');
+
+p1
+	.then(output)
+	.then(function() {
+		return p2;
+	})
+	.then(output)
+	.then(function() {
+		return p3;
+	})
+	.then(output)
+	.then(function() {
+		output('complete');
+	});
