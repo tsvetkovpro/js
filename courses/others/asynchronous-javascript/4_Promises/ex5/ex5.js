dedcbb26c0a84444f2f5d7402579c967565957b9
tsvetkovpro <285019@gmail.com>
2017-02-04 09:53:27 +0500

    asynchronous-javascript - 4_Promises
     - Exercise_5

---
+++ courses/others/asynchronous-javascript/4_Promises/ex5/ex5.js
@@ -0,0 +1,36 @@
+function fakeAjax(url, cb) {
+	var fake_responses = {
+		"file1": "The first text",
+		"file2": "The middle text",
+		"file3": "The last text"
+	};
+	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+
+	console.log("Requesting: " + url);
+
+	setTimeout(function() {
+		cb(fake_responses[url]);
+	}, randomDelay);
+}
+
+function output(text) {
+	console.log(text);
+}
+
+// **************************************
+
+function getFile(file) {
+	return ASQ(function(done) {
+		fakeAjax(file, done);
+	});
+}
+
+getFile('file1')
+	.val(output)
+	.seq(getFile('file2'))
+	.val(output)
+	.seq(getFile('file3'))
+	.val(output)
+	.val(function() {
+		output('complete');
+	});
